dfdbceeb5919a79ff96916fad8810260fa9f7798
Huunhat123 <huunhat20052006@gmail.com>
2024-11-20 15:58:57 +0700

    nộpp bài session02-lession02

---
+++ Session02-02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	// khai bao bien
+	int num1,num2,total;
+	
+	// nhap gia tri cho bien num1 tu ban phim 
+	printf("Moi ban nhap gia tri thu nhat");
+	scanf("%d",&num1);
+	
+	// nhap gia tri cho bien num2 tu ban phim 
+	printf("Moi ban nhap gia tri thu hai");
+	scanf("%d",&num2);
+	
+	total = num1 + num2;
+	
+	 printf("%d + %d =%d",num1,num2,total);
+	 
+	 
+	 return 0; 
+	
+} 
