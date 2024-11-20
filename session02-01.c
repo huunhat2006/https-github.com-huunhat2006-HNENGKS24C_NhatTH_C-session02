8f88e2a499522eb53fe4e31a5c1e0f2e1be5af9c
Huunhat123 <huunhat20052006@gmail.com>
2024-11-20 15:58:20 +0700

    nộp bài session01- lession01

---
+++ session02-01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+  // int la kieu du lieu so nguyen 
+    int a = 1;
+	
+  // fload va double la kieu du lieu so thap phan nhung double co kich thuoc luu tru lon hon
+    float sum = 3.3;
+    double b = 3.3333333333;
+  // char la kieu du lieu ki tu
+    char a 
+	 
